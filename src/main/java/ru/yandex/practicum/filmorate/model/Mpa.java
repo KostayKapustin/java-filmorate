67002005078cb1be6e7a653c33bd18bd3f5918f5
Костя Капустин <98394512+KostayKapustin@users.noreply.github.com>
2022-08-14 11:23:19 +0300

    ТЗ-11 добавлены тесты

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -11,11 +11,11 @@ public class Mpa {
     private int id;
     private String name;
 
-    public Mpa( int id) {
+    public Mpa(int id) {
         this.id = id;
     }
 
-    public Mpa( int id, String name) {
+    public Mpa(int id, String name) {
         this.id = id;
         this.name = name;
     }
